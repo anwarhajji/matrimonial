2b3fb183de1acbcabbf213dcc118dd0557a5ead1
anwarhajji <anwar.hajji@gmail.com>
2024-07-18 13:14:25 +0000

    theme and form color set ok

--- components/matches/Card.tsx
+++ components/matches/Card.tsx
@@ -24,16 +24,16 @@ const Card = ({ id }: Props) => {
         />
       </div>
       <div className="flex-[.1] px-2 py-2 flex justify-between items-center">
-        <EnvelopeIcon className="w-6 h-6 text-primary hover:text-secondary cursor-pointer" />
-        <FireIcon className="w-6 h-6 hover:text-secondary text-primary cursor-pointer" />
-        <EyeIcon className="w-6 h-6 hover:text-secondary text-primary cursor-pointer" />
-        <StarIcon className="w-6 h-6 hover:text-secondary text-primary cursor-pointer z-10" />
+        <EnvelopeIcon className="w-6 h-6 text-primary hover:text-white cursor-pointer" />
+        <FireIcon className="w-6 h-6 hover:text-primary text-white cursor-pointer" />
+        <EyeIcon className="w-6 h-6 hover:text-primary text-white cursor-pointer" />
+        <StarIcon className="w-6 h-6 hover:text-primary text-white cursor-pointer z-10" />
       </div>
       <div className="flex-[.1] bg-gradient-to-r from-indigo-500 to-purple-500  absolute bottom-0 left-0 opacity-0 group-hover:opacity-100 group-hover:bottom-10 h-12 group-hover:h-[180px] w-full transition-all duration-500 ease-out rounded p-2 flex flex-col gap-2">
         <h1 className="font-extrabold text-lg text-dark tracking-wide">
           *********
         </h1>
-        <p className="text-sm tracking-wide text-primary">Welcome</p>
+        <p className="text-sm tracking-wide text-white">Welcome</p>
         <p className="text-md text-dark tracking-wide">Please register</p>
         <p className="text-md text-dark tracking-wide">to meet your soulmate</p>
         <p className="text-md text-dark tracking-wide">********</p>
