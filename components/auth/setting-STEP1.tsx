7db260a83048cb5310a14bcfede142db5b9b7750
anwarhajji <anwar.hajji@gmail.com>
2024-07-21 07:28:10 +0000

    add stripe not cpmleted jjjO

--- components/auth/setting-STEP1.tsx
+++ components/auth/setting-STEP1.tsx
@@ -93,7 +93,6 @@ export const SettigStep1 = ({
                 return (
                   <Input
                     defaultValue={user.fullname!}
-                    isRequired
                     label="fullname"
                     type="text"
                     disabled={isPending}
